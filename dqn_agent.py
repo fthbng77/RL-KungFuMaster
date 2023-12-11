6f0d51f679d72cbdafc1921f95ff9307ab486676
fthbng77 <fatihbing38@gmail.com>
2023-12-12 00:39:31 +0300

    made more modular

---
+++ dqn_agent.py
@@ -0,0 +1,86 @@
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.activations import relu, linear
+from tensorflow.keras.losses import mean_squared_error
+from tensorflow.keras.optimizers import Adam
+import random
+from collections import deque
+
+class DQN:
+    def __init__(self, env, lr, gamma, epsilon, epsilon_decay):
+        self.env = env
+        self.lr = lr
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.epsilon_decay = epsilon_decay
+        self.replay_memory_buffer = deque(maxlen=500000)
+        self.batch_size = 64
+        self.epsilon_min = 0.01
+        self.num_action_space = env.action_space.n
+        self.num_observation_space = np.prod(env.observation_space.shape)
+        self.model = self.initialize_model()
+
+    def initialize_model(self):
+        model = Sequential()
+        model.add(Dense(512, input_dim=self.num_observation_space, activation=relu))
+        model.add(Dense(256, activation=relu))
+        model.add(Dense(self.num_action_space, activation=linear))
+        model.compile(loss=mean_squared_error, optimizer=Adam(lr=self.lr))
+        return model
+
+    def get_action(self, state):
+        if np.random.rand() < self.epsilon:
+            return random.randrange(self.num_action_space)
+        predicted_actions = self.model.predict(state)
+        return np.argmax(predicted_actions[0])
+
+    def add_to_replay_memory(self, state, action, reward, next_state, done):
+        self.replay_memory_buffer.append((state, action, reward, next_state, done))
+
+    def learn_and_update_weights_by_reply(self):
+        if len(self.replay_memory_buffer) < self.batch_size:
+            return
+
+        random_sample = self.get_random_sample_from_replay_mem()
+        states, actions, rewards, next_states, done_list = self.get_attribues_from_sample(random_sample)
+        targets = rewards + self.gamma * (np.amax(self.model.predict_on_batch(next_states), axis=1)) * (1 - done_list)
+        target_vec = self.model.predict_on_batch(states)
+        indexes = np.array([i for i in range(self.batch_size)])
+        target_vec[[indexes], [actions]] = targets
+        self.model.fit(states, target_vec, epochs=1, verbose=0)
+
+    def get_attribues_from_sample(self, random_sample):
+        states = np.array([i[0] for i in random_sample])
+        actions = np.array([i[1] for i in random_sample])
+        rewards = np.array([i[2] for i in random_sample])
+        next_states = np.array([i[3] for i in random_sample])
+        done_list = np.array([i[4] for i in random_sample])
+        states = np.squeeze(states)
+        next_states = np.squeeze(next_states)
+        return states, actions, rewards, next_states, done_list
+
+    def get_random_sample_from_replay_mem(self):
+        return random.sample(self.replay_memory_buffer, self.batch_size)
+
+    def train(self, num_episodes):
+        rewards_list = []
+        for episode in range(num_episodes):
+            state = self.env.reset()
+            state = np.reshape(state, [1, self.num_observation_space])
+            total_reward = 0
+            done = False
+            while not done:
+                action = self.get_action(state)
+                next_state, reward, done, _ = self.env.step(action)
+                next_state = np.reshape(next_state, [1, self.num_observation_space])
+                self.add_to_replay_memory(state, action, reward, next_state, done)
+                state = next_state
+                total_reward += reward
+                self.learn_and_update_weights_by_reply()
+                if done:
+                    break
+            rewards_list.append(total_reward)
+            self.epsilon = max(self.epsilon_min, self.epsilon * self.epsilon_decay)
+            print(f"Episode: {episode}, Total Reward: {total_reward}, Epsilon: {self.epsilon}")
+        return rewards_list
---
+++ main.py
@@ -0,0 +1,55 @@
+from dqn_agent import DQN
+from utils import plot_df, save_model, load_trained_model
+import gym
+import numpy as np
+import config 
+import pickle
+import pandas as pd
+
+def main():
+    # Gym ortamını başlat
+    env = gym.make('ALE/KungFuMaster-v5', render_mode="rgb_array")
+    env.action_space.seed(42)
+    np.random.seed(21)
+
+    # DQN modelini başlat
+    model = DQN(env, config.lr, config.gamma, config.epsilon, config.epsilon_decay)
+    
+    print("Starting training for DQN model...")
+    model.train(config.training_episodes, True)
+
+    # Modeli kaydet
+    save_dir = "saved_models/"
+    save_model(model, save_dir + "trained_model.h5")
+
+    # Eğitim ödüllerini kaydet ve görselleştir
+    pickle.dump(model.rewards_list, open(save_dir + "train_rewards_list.p", "wb"))
+    rewards_list = pickle.load(open(save_dir + "train_rewards_list.p", "rb"))
+    reward_df = pd.DataFrame(rewards_list)
+    plot_df(reward_df, save_dir + "training_rewards.png", "Training Rewards per Episode", "Episode", "Reward")
+
+    # Eğitilmiş modeli test et
+    trained_model = load_trained_model(save_dir + "trained_model.h5")
+    test_rewards = test_already_trained_model(trained_model, env)
+    pickle.dump(test_rewards, open(save_dir + "test_rewards.p", "wb"))
+    test_rewards = pickle.load(open(save_dir + "test_rewards.p", "rb"))
+    plot_df(pd.DataFrame(test_rewards), save_dir + "testing_rewards.png", "Testing Rewards per Episode", "Episode", "Reward")
+
+    print("Training and Testing Completed!")
+
+def test_already_trained_model(trained_model, env, num_episodes=100):
+    test_rewards = []
+    for episode in range(num_episodes):
+        state = env.reset()
+        total_reward = 0
+        done = False
+        while not done:
+            action = np.argmax(trained_model.predict(state.reshape(1, -1))[0])
+            state, reward, done, _ = env.step(action)
+            total_reward += reward
+        test_rewards.append(total_reward)
+        print(f"Episode: {episode}, Total Reward: {total_reward}")
+    return test_rewards
+
+if __name__ == "__main__":
+    main()
